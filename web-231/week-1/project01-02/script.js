7f8243a4f01b400e6f0caae2fa3a184f46971be2
Joe <akindelejoe@yahoo.com>
2025-02-09 14:58:42 -0600

    Added week-1 with project files

---
+++ web-231/week-1/project01-02/script.js
@@ -0,0 +1,18 @@
+document.getElementById("blanket-link").onclick = function () {
+  document.getElementById("plantDescription").textContent = 
+    "This plant is called Blanket. It's beautiful and adds color to the garden.";
+};
+
+document.getElementById("bluestem-link").onclick = function () {
+  document.getElementById("Description").textContent = 
+    "This is called Bluestem. A strong plant and a good addition to the garden.";
+};
+
+document.getElementById("rugosa-link").onclick = function () {
+  document.getElementById("Details").textContent = 
+    "Rugosa! It's unique and vibrant!";
+};
+
+  
+
+  
